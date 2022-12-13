7f2b5ff30eadaf8e544e5b5ffd7331ca98a60510
Ndi-Shema <f.shema@alustudent.com>
2022-12-13 12:44:27 +0000

    Strace is your friend

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 #how to load a php file
 exec {'replacing_right path':
   command => 'sed -i "s/.phpp/.php/g"  /var/www/html/wp-settings.php',
-  pathway    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   onlyif  => 'test -f /var/www/html/wp-settings.php'
 }
